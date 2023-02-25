30e9d6caebc89755829402732cbc66422f84ef06
Vishvas <visuhadiyal2002@gmail.com>
2023-02-25 12:22:12 +0530

    adding pr01 files

---
+++ react-project01/src/components/Expenseitem.js
@@ -0,0 +1,23 @@
+import React,{useState}from "react";
+import "./Expenseitem.css";
+import Date from "./Expensedate";
+
+const Expenseitem = (props) => {
+ const [title,setTitle]=useState(props.title)
+  const clicked = ()=>{
+    setTitle('Updated!')
+    console.log(title,setTitle);
+  }
+  return (
+    <div className="expense-item">
+      <Date date={props.date}></Date>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{props.amount}$</div>
+      </div>
+      <button onClick={clicked}>change</button>
+    </div>
+  );
+};
+
+export default Expenseitem;
