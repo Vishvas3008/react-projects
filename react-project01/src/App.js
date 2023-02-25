1dc09709d09d30a00179926b79002c1a465b1816
Vishvas <globalUser2023@gmail.com>
2023-02-25 12:55:38 +0530

    commit--

--- react-project01/src/App.js
+++ react-project01/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import Expenses from "./components/Expenses.js";
+import Expenses from "./components/Expenses/Expenses.js";
 const App = () => {
   const expenses = [
     {
